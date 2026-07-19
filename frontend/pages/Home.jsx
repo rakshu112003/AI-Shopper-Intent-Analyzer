f2927b1de3d7dd685831ea9447a619984ed2e56d
Rakshitha HN <rakshug73@gmail.com>
2026-07-19 19:19:30 +0530

    Create Home.jsx

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#2563eb",
+        color: "white",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h1>🛍️ AI Shopper Intent Analyzer</h1>
+      <p>Analyze customer shopping behavior using AI</p>
+    </header>
+  );
+}
+
+export default Header;
