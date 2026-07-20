d61c7accdafe8711c57577e8ad53df236b322828
Rakshitha HN <rakshug73@gmail.com>
2026-07-20 08:05:23 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import EventInput from "../components/EventInput";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <EventInput />
+    </div>
+  );
+}
+
+export default Home;
